644b016caeae60ebc1f585a029d3d5eaaa0a4bdd
tangsanrich <richnosep@gmail.com>
2025-12-30 12:34:24 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"fmt"
-	
-	// SEKARANG INI PASTI BISA (Karena induknya sudah ada):
 	"github.com/AllenDang/cimgui-go/imgui"
 )
 
 func main() {
 	fmt.Println("[+] Memulai Aplikasi...")
 
-	// Perhatikan: Kita panggil pakai 'imgui.' bukan 'cimgui.'
+	// Buat context
 	ctx := imgui.CreateContext()
-	defer imgui.DestroyContext(ctx)
+	
+	// PERBAIKAN DI SINI: Hapus 'ctx' dari dalam kurung
+	defer imgui.DestroyContext()
 	
 	io := imgui.CurrentIO()
 	io.SetDisplaySize(imgui.Vec2{X: 1080, Y: 2400}) 
 	
 	fmt.Println("[SUCCESS] Context ImGui berhasil dibuat!")
-	fmt.Println("[INFO] Build sukses menggunakan package imgui.")
+	fmt.Println("[INFO] Build sukses! Tidak ada lagi error.")
 }
 
